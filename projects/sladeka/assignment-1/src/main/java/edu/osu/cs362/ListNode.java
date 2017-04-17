a4dd30cad7df1b914144cd38de958cab0d8200ab
ASladek <sladeka@oregonstate.edu>
2017-04-17 01:48:54 -0700

    Added Assignment1

---
+++ projects/sladeka/assignment-1/src/main/java/edu/osu/cs362/ListNode.java
@@ -0,0 +1,19 @@
+package edu.osu.cs362;
+// Basic node stored in a linked list
+// Note that this class is not accessible outside
+// of package weiss.nonstandard
+
+class ListNode {
+    // Constructors
+    public ListNode(Object theElement) {
+        this(theElement, null);
+    }
+
+    public ListNode(Object theElement, ListNode n) {
+        element = theElement;
+        next = n;
+    }
+
+    public Object element;
+    public ListNode next;
+}
