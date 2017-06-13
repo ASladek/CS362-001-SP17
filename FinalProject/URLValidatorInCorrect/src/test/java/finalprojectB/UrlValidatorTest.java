ffc93f6a1fe5c358ae338bc9de19302e0cb9eea5
ASladek <sladeka@oregonstate.edu>
2017-06-12 23:50:20 -0700

    testing for URL validator

--- FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -41,30 +41,73 @@ public class UrlValidatorTest extends TestCase {
    public void testManualTest()
    {
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   System.out.println("testManualTest: True");
 	   System.out.println(urlVal.isValid("http://www.amazon.com"));
-	   
-	   
+       System.out.println(urlVal.isValid("http://www.google.com/awesome"));
+       System.out.println(urlVal.isValid("http://somewhere.com/pathxyz/file(1).html"));
+
+       System.out.println("testManualTest: False");
+       System.out.println(urlVal.isValid("qoewjfpdsc"));
+	   System.out.println(urlVal.isValid("notvalid//com"));
+       System.out.println(urlVal.isValid("file:///C:\\some.file"));
+       System.out.println(urlVal.isValid("asdf"));
    }
    
-   
+   //Tests valid urls and expects them to be found valid
    public void testYourFirstPartition()
-   {
-	   
-   }
-   
+       {
+       UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+       System.out.println("testTruePartition: True");
+       System.out.println(urlVal.isValid("http://www.amazon.com"));
+       System.out.println(urlVal.isValid("http://www.google.com/awesome"));
+       System.out.println(urlVal.isValid("http://somewhere.com/pathxyz/file(1).html"));
+       System.out.println(urlVal.isValid("http://www.seoul.co.kr/"));
+
+
+
+       }
+
+   //Test invalid urls and expect them to be found invalid
    public void testYourSecondPartition(){
-	   
+       UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+
+       System.out.println("testFalsePartition: False");
+       System.out.println(urlVal.isValid("qoewjfpdsc"));
+       System.out.println(urlVal.isValid("notvalid//com"));
+       System.out.println(urlVal.isValid("file:///C:\\some.file"));
+       System.out.println(urlVal.isValid("oie ejropgl aerpog"));
+       System.out.println(urlVal.isValid("http://300.40.5.0"));
+
+
    }
-   
-   
+
+
+   //Call generated urls, expect them to be valid if all parts are valid
+    /*
    public void testIsValid()
    {
-	   for(int i = 0;i<10000;i++)
-	   {
-		   
-	   }
+       UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+       String testURL;
+       boolean testValid = true;
+       int ischeme;
+       int iauth;
+       int ipath;
+       int iq;
+
+       for(int i = 0;i<10000;i++)
+       {
+           ischeme = (int)(Math.random()*4);
+           iauth = (int)(Math.random()*5);
+           ipath = (int)(Math.random()*2);
+           iq = (int)(Math.random()*2);
+           System.out.println( String(Uscheme[ischeme][0]) + String(Uauthority[iauth][0]) + String(Upath[ipath][0]) + String(Uquery[iq][0]));
+
+
+
+       }
    }
-   
+*/
+   //
    public void testAnyOtherUnitTest()
    {
 	   
@@ -75,6 +118,32 @@ public class UrlValidatorTest extends TestCase {
     *
     * @param testObjects Used to create a url.
     */
-   
+   private Object[][] Uscheme = new Object[][]{
+           {"http://", true},
+           {"http:", false},
+           {"ftp://", true},
+           {"", true}
+   };
+
+   private Object[][] Uauthority = new Object[][]{
+           {"www.google.com", true},
+           {"oregonstate.edu", true},
+           {"0.0.0.0", true},
+           {"www.google.com:80", true},
+           {"aaa.", false}
+   };
+
+    private Object[][] Upath = new Object[][]{
+            {"/test",true},
+            {"/..", false}
+    };
+
+    private Object[][] Uquery = new Object[][]{
+            {"?action=view", true},
+            {"", true}
+    };
+
+    private Object[] buildURL = {Uscheme, Uauthority, Upath, Uquery};
+
 
 }
